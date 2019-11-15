ebf1f0e0d230df30905dfad73940c7d2dbea62be
TACKHYUN JUNG <41291493+takhyun12@users.noreply.github.com>
2019-11-15 12:33:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-# main 소스코드 재개발 중입니다..
+# Protocol State Inference pseudo Code
+
+Input : Message msg, Field_size fs, Buffer buf
+
+buf = [sum(fs)]
+state = False
+while(msg):
+    if msg.arrive():
+        buf = [1] * msg.arrive_size()
+        if buf.is_Full():
+            state = True # Received
+            break;
+        else:
+            state = False # Receiving
+           
+Output : state (True/False)
